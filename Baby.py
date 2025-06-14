03a2e8c8d75425c9d9e7ca2c5f5427cc92fcf35a
Patryk Gluszkiewicz <gluszkiewiczp@cardiff.ac.uk>
2025-06-14 17:42:07 +0100

    Complete babysitting process

--- Baby.py
+++ Baby.py
@@ -3,7 +3,7 @@ import pygame
 from GameObject import GameObject
 
 class Baby(GameObject):
-    def __init__(self, controller, color=(255, 255, 255), width=50, height=50, x=0, y=0, tired=True, sleep_required=10):
+    def __init__(self, controller, color=(255, 255, 255), width=50, height=50, x=0, y=0, tired=True, sleep_required=5):
         super().__init__(
             controller,
             color,
@@ -16,6 +16,7 @@ class Baby(GameObject):
         self.sleep_required = sleep_required
         self.pick_up_sound = pygame.mixer.Sound("PickUpBaby.wav")
         self.sleep_sound = pygame.mixer.Sound("BabySleep.wav")
+        self.wake_sound = pygame.mixer.Sound("BabyWake.wav")
 
     def tired(self):
         if (pygame.time.get_ticks() - self.sleep_start) / 1000 >= self.sleep_required:
--- BabyBed.py
+++ BabyBed.py
@@ -14,6 +14,8 @@ class BabyBed(GameObject):
         )
         self.babies = []
         self.capacity = baby_limit
+        self.collect_money = False
+        self.money_sound = pygame.mixer.Sound("Money.wav")
 
     def add_baby(self, baby):
         if len(self.babies) < self.capacity:
@@ -26,6 +28,10 @@ class BabyBed(GameObject):
         bed_space = len(self.babies) < self.capacity
         niamh_space = len(self.controller.niamh.babies) < self.controller.niamh.capacity
 
+        if collision and self.collect_money:
+            self.collect_money = False
+            self.money_sound.play()
+
         # Put 1 baby into the bed
         if collision and carrying and bed_space:
             baby = self.controller.niamh.remove_baby()
@@ -35,8 +41,17 @@ class BabyBed(GameObject):
             if self.babies[0].awake(self.controller.niamh):
                 del self.babies[0]
 
-        if len(self.babies) > 0:
+        if not bed_space:
+            if not self.babies[0].tired():
+                self.babies[0].wake_sound.play()
+                del self.babies[0]
+                self.collect_money = True
+
+        if self.collect_money:
+            self.color = (255, 255, 0)
+
+        elif len(self.babies) > 0:
             self.color = (255, 255, 255)
 
-        if len(self.babies) == 0:
+        elif len(self.babies) == 0:
             self.color = (128, 32, 32)
